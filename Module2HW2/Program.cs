28f2b29cfc0aab79b52646a2bafac4af785a4eb3
AngrySonya <Photographersofi@gmail.com>
2021-07-11 14:48:51 +0300

    Initial commit

---
+++ Module2HW2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Module2HW2
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
